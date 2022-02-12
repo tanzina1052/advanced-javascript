155f8d7a7bb48be32e337a5a49e410324ebdfda8
tanzina1052 <tsultana537@gmail.com>
2022-02-12 23:51:49 +0600

    modified in one place

--- arguments.js
+++ arguments.js
@@ -3,7 +3,7 @@
 function add(num1,num2) {
     console.log(arguments);
     console.log([...arguments]);
-    return num1 + num2;
+    return num1 + num2 + arguments[2];
      
 }
 const result = add(5,5,5,7);
